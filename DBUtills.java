bfb8c89535b1bc1035958102c0f4527317669628
jagadishkumarbandi <jagadishkumar.bandi@gmail.com>
2022-01-09 05:22:13 +0530

    update

--- DBUtills.java
+++
@@ -1,7 +0,0 @@
-This is update code in devlopment branch1.
-
-This is a update in master branch
-Hello Guys this is jagadish kumar , Good afternoon.
-Git Commands are very importinta for practice
-We are adding the file in master file 
-
